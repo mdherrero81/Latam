d5b59899891c6d6d3e9ddd7e1c07e08916fea6ea
mdherrero81 <95425606+mdherrero81@users.noreply.github.com>
2025-02-02 14:35:09 -0300

    actualizacion challenger

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const listaAmigos = [];
+
+//funcion para agregar un amigo
+function agregarAmigo() {
+    const nameInput = document.getElementById('amigo');
+    const name = nameInput.value.trim();
+    if (name) {
+        listaAmigos.push(name);
+        updateNameList();
+        nameInput.value = '';
+    }
+}
+
+function updateNameList() {
+    const nameListElement = document.getElementById('listaAmigos');
+    nameListElement.innerHTML = '';
+    listaAmigos.forEach((name, index) => {
+        const li = document.createElement('li');
+        li.textContent = name;
+        nameListElement.appendChild(li);
+    });
+}
+
+//funcion para elegir al azzar
+function sortearAmigo() {
+    if (listaAmigos.length > 0) {
+        const randomIndex = Math.floor(Math.random() * listaAmigos.length);
+        const selectedName = listaAmigos[randomIndex];
+        document.getElementById('resultado').textContent = `Nombre seleccionado: ${selectedName}`;
+    } else {
+        document.getElementById('resultado').textContent = 'No hay nombres en la lista.';
+    }
+}
+
